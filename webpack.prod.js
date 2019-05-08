72b92d3f753a4f88faeeff7c3995a64045caafb2
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2019-05-09 00:13:13 +0700

    pure react

---
+++ webpack.prod.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/bundle'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ]
+};
