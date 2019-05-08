bf72cfea630572bb248d092f75baefab550ac1b4
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2019-05-09 00:03:54 +0700

    pure react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello 12
+      </div>
+    );
+  }
+}
+
+export default App;
